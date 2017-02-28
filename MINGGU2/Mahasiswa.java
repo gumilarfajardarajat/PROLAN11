1bac7546bc26204afa13c693b2f049fda79385b0
Gumilar Fajar Darajat <gumilarfajardarajat@gmail.com>
2017-02-28 21:03:41 +0700

    Add files via upload

---
+++ MINGGU2/Mahasiswa.java
@@ -0,0 +1,7 @@
+public class Mahasiswa{
+	public static void main(String[] args){
+		System.out.println("Nama Lengkap : Gumilar Fajar Darajat");
+		System.out.println("NIM : 10114200");
+		System.out.println("Alamat : Pangandaran");
+	}
+}
